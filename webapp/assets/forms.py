aef05431ca1a816d81c1875af92231c80f872e29
Evgeny S <e-sizhkov@ya.ru>
2022-01-25 18:26:39 +0300

    Fix input 0 in Number

--- webapp/assets/forms.py
+++ webapp/assets/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 from webapp.assets.models import Asset
 
 
 class AssetSelection(FlaskForm):
     asset = StringField('Select Share / Bond / ETF', validators=[DataRequired()],
                         render_kw={"class": "form-control", "placeholder": "Select Share / Bond / ETF..."})
-    number = IntegerField('Number', validators=[DataRequired()],
+    number = IntegerField('Number', validators=[InputRequired()],
                           render_kw={"class": "form-control", "placeholder": "Number..."})
     submit = SubmitField('Add', render_kw={"class": "btn btn-primary btn-block"})
 
     def validate_number(self, number):
-        if number.data <= 0:
+        if number.data < 0:
             raise ValidationError('Number must be greater than 0')
 
     def validate_asset(self, asset):
